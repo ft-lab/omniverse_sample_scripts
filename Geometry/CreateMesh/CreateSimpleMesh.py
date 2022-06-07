5daa30ab1032f1bfdf3be5752dcf35ddce8ec978
yoshisaka <y_yutaka@fa2.so-net.ne.jp>
2022-06-07 12:57:15 +0900

    Geometry-CreateMesh-CreateSimpleMesh.py を調整
    Interpolationの指定を明示、
    Subdivisionは明示的に"none"を指定。

--- Geometry/CreateMesh/CreateSimpleMesh.py
+++ Geometry/CreateMesh/CreateSimpleMesh.py
@@ -3,14 +3,23 @@ from pxr import Usd, UsdGeom, UsdPhysics, UsdShade, Sdf, Gf, Tf
 # Get stage.
 stage = omni.usd.get_context().get_stage()
 
+# Get default prim.
+defaultPrim = stage.GetDefaultPrim()
+
+# Get root path.
+rootPath = '/'
+if defaultPrim.IsValid():
+    rootPath = defaultPrim.GetPath().pathString
+
 # Create mesh.
-meshGeom = UsdGeom.Mesh.Define(stage, "/World/mesh")
+meshGeom = UsdGeom.Mesh.Define(stage, rootPath + "/mesh")
 
 # Set vertices.
 meshGeom.CreatePointsAttr([(-10, 0, -10), (-10, 0, 10), (10, 0, 10), (10, 0, -10)])
 
 # Set normals.
 meshGeom.CreateNormalsAttr([(0, 1, 0), (0, 1, 0), (0, 1, 0), (0, 1, 0)])
+meshGeom.SetNormalsInterpolation("vertex")
 
 # Set face vertex count.
 meshGeom.CreateFaceVertexCountsAttr([4])
@@ -21,6 +30,8 @@ meshGeom.CreateFaceVertexIndicesAttr([0, 1, 2, 3])
 # Set uvs.
 texCoords = meshGeom.CreatePrimvar("st", 
         Sdf.ValueTypeNames.TexCoord2fArray, 
-        UsdGeom.Tokens.varying)
+        UsdGeom.Tokens.vertex)
 texCoords.Set([(0, 1), (0, 0), (1, 0), (1, 1)])
 
+# Subdivision is set to none.
+meshGeom.CreateSubdivisionSchemeAttr().Set("none")
