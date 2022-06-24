03b3bed87bc235302643f200e43957fbca2d0f17
yoshisaka <y_yutaka@fa2.so-net.ne.jp>
2022-06-24 23:46:33 +0900

    AssetConverter-importObjToUSD.pyにオプション指定の追加

--- AssetConverter/importObjToUSD.py
+++ AssetConverter/importObjToUSD.py
@@ -1,3 +1,7 @@
+# --------------------------------------------------.
+# obj to usd conversion.
+# See : https://docs.omniverse.nvidia.com/app_create/prod_extensions/ext_asset-converter.html
+# --------------------------------------------------.
 import carb
 import omni
 import asyncio
@@ -12,16 +16,21 @@ def progress_callback (current_step: int, total: int):
 async def convert_asset_to_usd (input_asset: str, output_usd: str):
    # Input options are defaults.
    converter_context = omni.kit.asset_converter.AssetConverterContext()
-   #converter_context.keep_all_materials = True
-   #converter_context.export_preview_surface = False
-   #converter_context.ignore_animations = False
-   #converter_context.ignore_camera = False
-   #converter_context.ignore_light = False
-   #converter_context.smooth_normals = True
-   #converter_context.use_meter_as_world_unit = False
-   #converter_context.create_world_as_default_root_prim = True
-   #converter_context.merge_all_meshes = False
-
+   converter_context.ignore_materials = False
+   converter_context.ignore_camera = False
+   converter_context.ignore_animations = False
+   converter_context.ignore_light = False
+   converter_context.export_preview_surface = False
+   converter_context.use_meter_as_world_unit = False
+   converter_context.create_world_as_default_root_prim = True
+   converter_context.embed_textures = True
+   converter_context.convert_fbx_to_y_up = False
+   converter_context.convert_fbx_to_z_up = False
+   converter_context.merge_all_meshes = False
+   converter_context.use_double_precision_to_usd_transform_op = False 
+   converter_context.ignore_pivots = False 
+   converter_context.keep_all_materials = True
+   converter_context.smooth_normals = True
    instance = omni.kit.asset_converter.get_instance()
    task = instance.create_converter_task(input_asset, output_usd, progress_callback, converter_context)
 
