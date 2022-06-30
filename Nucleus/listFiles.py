1bba8b849b9e48f3ba76e2286763f0359825279d
yoshisaka <y_yutaka@fa2.so-net.ne.jp>
2022-06-30 11:37:52 +0900

    Nucleus-listFiles.py を追加

---
+++ Nucleus/listFiles.py
@@ -0,0 +1,52 @@
+import asyncio
+import omni.client
+
+# Lists files in the specified path.
+async def listFiles (_depth : int, _path : str):
+    (result, entries) = await omni.client.list_async(_path)
+    if result != omni.client.Result.OK:
+        return
+
+    for entry in entries:
+        # Skip for Channel file.
+        if (entry.flags & omni.client.ItemFlags.IS_CHANNEL) != 0:
+            continue
+
+        name = entry.relative_path
+        if name == "" or name.find(".") == 0:
+            continue
+
+        path = _path + "/" + name
+
+        isFolder = (entry.flags & omni.client.ItemFlags.CAN_HAVE_CHILDREN) != 0
+
+        msg = ""
+        for i in range(_depth):
+            msg += "  "
+
+        if isFolder:
+            msg += "[" + name + "]"
+        else:
+            msg += name
+        
+        msg += "   "
+        if (entry.flags & omni.client.ItemFlags.READABLE_FILE) != 0:
+            msg += " [R]"
+        if (entry.flags & omni.client.ItemFlags.WRITEABLE_FILE) != 0:
+            msg += " [W]"
+        if (entry.flags & omni.client.ItemFlags.CAN_LIVE_UPDATE) != 0:
+            msg += " [LiveUpdate]"
+
+        if not isFolder:
+            msg += f" {entry.size} bytes"
+
+        print(msg)
+
+        if isFolder:
+            await listFiles(_depth + 1, path)
+
+# -----------------------------------------------.
+
+path1 = "omniverse://localhost/Users/test"
+asyncio.ensure_future(listFiles(0, path1))
+
--- Nucleus/readme.md
+++ Nucleus/readme.md
@@ -2,10 +2,16 @@
 
 OmniverseではUSDからの保存はNucleusの指定のパス(omniverse://localhost/xxx/yyy.usd)に出力できます。    
 別途、Nucleus上でファイル操作を行う「omni.client」が用意されています。     
+omni.clientの機能はコルーチンから呼ぶ必要があります。     
+
+## 参考
+
+https://docs.omniverse.nvidia.com/py/kit/source/extensions/omni.client/docs/index.html
 
 ## サンプル
 
 |ファイル|説明|     
 |---|---|     
-|[existPath.py](./existPath.py)|Nucleus上のファイルまたはフォルダが存在するかチェックします|     
+|[existPath.py](./existPath.py)|Nucleus上のファイルまたはフォルダが存在するかチェック。|     
+|[listFiles.py](./listFiles.py)|Nucleus上の指定のフォルダ内のファイルを一覧。|     
 
