9788df8d05984cc222bfb5fa815c105a51d13ff6
yoshisaka <y_yutaka@fa2.so-net.ne.jp>
2022-07-11 09:39:44 +0900

    Nucleus-existPath.pyで結果を待つ例を追加

--- Nucleus/existPath.py
+++ Nucleus/existPath.py
@@ -12,6 +12,17 @@ async def existPath (path : str):
 
     print("Exist : " + path)
 
+# 
+async def _ocl_existPath (path : str):
+    (result, entry) = await omni.client.stat_async(path)
+    if result == omni.client.Result.ERROR_NOT_FOUND:
+        return False
+    return True
+
+def ocl_existPath (path : str):
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(_ocl_existPath(path))
+
 # ------------------------------------------.
 path1 = "omniverse://localhost/Users/test"
 asyncio.ensure_future(existPath(path1))
@@ -19,3 +30,8 @@ asyncio.ensure_future(existPath(path1))
 path2 = "omniverse://localhost/Users/test/xxx.usd"
 asyncio.ensure_future(existPath(path2))
 
+
+if ocl_existPath(path1):
+    print("Exist : " + path1)
+else:
+    print("Does not exist : " + path1)
